171bc92050a63f7912fcbd92daa6646f749f52e8
Pierre Baudin <pierrejean.baudin@hotmail.com>
2018-10-16 09:58:54 +0800

    add run python intro

---
+++ Runpython/hello_world.py
@@ -0,0 +1,14 @@
+def sayHello():
+    print("Hello, world!")
+
+'''
+Tips to reload file after modification
+
+in ipython3
+%load_ext autoreload
+%autoreload 2
+
+in python
+import importlib
+importlib.reload(function)
+'''
