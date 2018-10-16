c544dc5691d0dc13b81b8d776a12952a1e160e44
Pierre Baudin <pierrejean.baudin@hotmail.com>
2018-10-16 15:13:23 +0800

    exception handling

--- Gettingstarted/basicscript.py
+++ Gettingstarted/basicscript.py
@@ -13,3 +13,23 @@ for name in student_names:
         print("Found him!\n" + name)
         break
     print("Currently testing " + name)
+
+# Dictionary example
+student = {
+    "name": "Mark",
+    "student_id": 15163,
+    "feedback": None
+}
+
+student["last_name"] = "Kowalski"
+
+# exception handling
+try:
+    last_name = student["last_name"]
+    numbered_last_name = 3 + last_name
+except KeyError:
+    print("Error finding last_name")
+except TypeError as error:
+    print("I cannot add these two together!")
+    print(error)
+print("This code executes!")
