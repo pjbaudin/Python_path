c2131b40eb1f6f0b14e61e9899b86ac675765e7f
Pierre Baudin <pierrejean.baudin@hotmail.com>
2018-10-17 12:11:33 +0800

    web app with Flask

--- pymodule/student.py
+++ pymodule/student.py
@@ -5,14 +5,16 @@ class Student:
 
 	school_name = "Springfield Elementary"
 
-	def __init__(self, name, student_id=332):
+	def __init__(self, name, student_id=332, last_name="NA"):
 		"""
 		Adds the student to the student list
 		:param name: string - student name
 		:param student_id: integer - optional student ID
+		:param last_name: string - optional student last name
 		"""
 		self.name = name
 		self.student_id = student_id
+		self.last_name = last_name
 		students.append(self)
 
 	def __str__(self):
