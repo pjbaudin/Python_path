5a5fffc5306846077b6b4dbe58780f654bd0ab50
Pierre Baudin <pierrejean.baudin@hotmail.com>
2018-10-24 10:00:07 +0800

    Iterable module

--- gen.py
+++ gen.py
@@ -24,5 +24,31 @@ def run_take():
         print(item)
 
 
+def distinct(iterable):
+    """
+    Return unique items by eliminating duplicates
+    :param iterable: the source series
+    :yields: unique elements in order from 'iterable'
+    """
+    seen = set()
+    for item in iterable:
+        if item in seen:
+            continue
+        yield item
+        seen.add(item)
+
+
+def run_distinct():
+    items = [5, 7, 7, 6, 5, 5]
+    for item in distinct(items):
+        print(item)
+
+
+def run_pipeline():
+    items = [3, 6, 6, 2, 1, 1]
+    for item in take(3, distinct(items)):
+        print(item)
+
+
 if __name__ == '__main__':
-    run_take()
+    run_pipeline()
