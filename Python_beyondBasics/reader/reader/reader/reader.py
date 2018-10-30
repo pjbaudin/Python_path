18093b915081450a892bf34478c6a1c177ef313b
Pierre Baudin <pierrejean.baudin@hotmail.com>
2018-10-30 10:06:41 +0800

    organizing larger program - tuto module

--- Python_beyondBasics/reader/reader/reader/reader.py
+++ Python_beyondBasics/reader/reader/reader/reader.py
@@ -1,11 +1,11 @@
 import os
+
 from reader.compressed import bzipped, gzipped
 
 extension_map = {
     '.bz2': bzipped.opener,
     '.gz': gzipped.opener,
 }
-
 
 class Reader:
     def __init__(self, filename):
