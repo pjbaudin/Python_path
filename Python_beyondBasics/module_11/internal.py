3d87cbc8d40512628e35e4cee12e85242792ccf4
Pierre Baudin <pierrejean.baudin@hotmail.com>
2018-11-08 11:55:06 +0800

    module on exceptions and errors handling

---
+++ Python_beyondBasics/module_11/internal.py
@@ -0,0 +1,9 @@
+def modulus_three(n):
+    r = n % 3
+    if r == 0:
+        print("Multiple of 3")
+    elif r == 1:
+        print("Remainder: 1")
+    else:
+        assert r == 2, "Remainder is not 2"
+        print("Remainder: 2")
